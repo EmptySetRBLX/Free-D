a0391d3d6339312ddec66214c7ccaf7e56352b2c
OTRD5k <otpony123@gmail.com>
2015-07-22 15:39:16 -0400

    Command Skeleton
    IGOR, ITS ALIVE!!!!!!!!!!!!!!!!!
    
    GAHAHAHAH!

--- ASM/Main.asm
+++ ASM/Main.asm
@@ -1,7 +1,34 @@
 alloc(mainmem,2048)
-alloc(stackdata, 1024)
+alloc(stackdata, 64)
+alloc(testcommand, 512)
+label(testcommandfalse)
 label(returnhere)
 label(cleanup)
+
+testcommand:
+	cmp [eax], 45455246 //FREE
+	jne testcommandfalse
+	cmp [eax+4], 5420442d //-D T
+	jne testcommandfalse
+	cmp [eax+8], 20545345 //EST 
+	jne testcommandfalse 
+	cmp [eax+c], 4d4d4f43 //COMM
+	jne testcommandfalse
+	cmp [eax+10], 414e4420 //AND 
+	
+	mov [eax], 57205449 //IT W
+	mov [eax+4], 454b524f //ORKE
+	mov [eax+8], 41592044 //D YA
+	mov [eax+c], 41414141 //AAAA
+	mov [eax+10], 59414141 //AAAY
+	
+	mov eax,1
+	
+	ret
+	
+	testcommandfalse:
+		mov eax, 0
+		ret
 
 mainmem: //this is allocated memory, you have read,write,execute access
 	mov [stackdata], eax
@@ -13,13 +40,18 @@ mainmem: //this is allocated memory, you have read,write,execute access
 	mov [stackdata+18], ebp
 	
 	pop eax
-	mov [stackdata+1c], eax
+	mov [stackdata+1c], eax //RETRUN ADDRESS
 	pop eax
-	mov [stackdata+20], eax
+	mov [stackdata+20], eax //DEST POINTER
 	pop eax
-	mov [stackdata+24], eax
+	mov [stackdata+24], eax //SOURCE POINTER
 	pop eax
-	mov [stackdata+28], eax
+	mov [stackdata+28], eax //LENGTH
+	
+	mov eax, esi
+	
+	call testcommand
+
 	
 	jmp cleanup
 
@@ -40,9 +72,14 @@ mainmem: //this is allocated memory, you have read,write,execute access
 		mov ecx, [stackdata+8]
 		mov ebx, [stackdata+4]
 		mov eax, [stackdata]
-		call RobloxStudioBeta.exe+9F9040
+		
+		cmp eax,[esi]
+		jne RobloxStudioBeta.exe+A8B6
+		add edx,04
 		jmp returnhere
 
-"RobloxStudioBeta.exe"+1698:
+"RobloxStudioBeta.exe"+A8A2:
 	jmp mainmem
+	nop
+	nop
 	returnhere:
---
+++ Plugin/Main.lua
@@ -0,0 +1,20 @@
+--Free-D mesh injection by OTRainbowDash5000/YAYZMAN23
+--Parser from a triangle obj loader by magnalite
+
+--Hastily cobbled together, wanted to push this out ASAP
+
+local toolbar = plugin:CreateToolbar("TESTA")
+
+local button = toolbar:CreateButton(
+	"TEST",
+	"Converts a .OBJ into an auto assembler script",
+	"http://www.roblox.com/asset/?id=270949662"
+)
+
+local test = Instance.new("StringValue")
+test.Value = "FREE-D" .. " TESTING"
+
+button.Click:connect(function()
+	test.Value = "FREE-D" .. " TEST COMMAND "
+	print(test.Value)
+end)
